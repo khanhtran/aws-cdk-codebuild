2b6649d5f0dc231f6e11ceb592337356ca71f47c
Tran, Khanh <trank@oclc.org>
2022-07-18 22:14:38 -0400

    init

--- lib/aws-cdk-codebuild-stack.ts
+++ lib/aws-cdk-codebuild-stack.ts
@@ -1,4 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage, Project, Source } from 'aws-cdk-lib/aws-codebuild';
+import { Repository, IRepository } from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +8,26 @@ export class AwsCdkCodebuildStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkCodebuildQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const repo: IRepository = Repository.fromRepositoryName(this, "sample-repo", "sample");
+    new Project(this, "SampleCodeBuild", {
+      source: Source.codeCommit({ repository: repo }),
+      projectName: 'my-sample-project',
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_5_0
+      },
+      buildSpec: BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'NUMBER=1',
+              'while [ $NUMBER -lt 5 ]; do',
+              'echo "Number is: $((NUMBER++))"',
+              'done'
+            ]
+          }
+        }
+      })
+    })
   }
 }
