b9ecb945d4675c0112d2c100c5d9c2e53542aa16
Tran, Khanh <trank@oclc.org>
2022-07-18 21:54:29 -0400

    Initial commit

---
+++ test/aws-cdk-codebuild.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkCodebuild from '../lib/aws-cdk-codebuild-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-codebuild-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkCodebuild.AwsCdkCodebuildStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
